10a3acaeb5693f3a00676b61ca94e5ee19db344b
noonbles <atrac480@gmail.com>
2024-08-04 16:07:43 -0400

    init commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,155 @@
-import Image from "next/image";
+"use client";
+
+import { React, useState, useEffect } from "react";
+import { List, ListItem, Stack, IconButton, TextField } from "@mui/material";
+import BreakfastDiningIcon from "@mui/icons-material/BreakfastDining";
+import EditIcon from "@mui/icons-material/Edit";
+import { firestore } from "./firebase.js";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  deleteDoc,
+  getDoc,
+} from "firebase/firestore";
 
 export default function Home() {
+  const [pantry, setPantry] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [newItem, setNewItem] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    async function fetchPantry() {
+      const pantryCollection = collection(firestore, "pantry");
+      const pantrySnapshot = await getDocs(pantryCollection);
+      const pantryItems = pantrySnapshot.docs.map((doc) => doc.id);
+      setPantry(pantryItems);
+    }
+    fetchPantry();
+  }, []);
+
+  const filteredPantry = pantry.filter((item) =>
+    item.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  async function addItem() {
+    let processed_txt = newItem.trim().toLowerCase();
+    if (processed_txt && !pantry.includes(processed_txt)) {
+      try {
+        const itemDoc = doc(firestore, "pantry", processed_txt);
+        await setDoc(itemDoc, { name: processed_txt });
+        setPantry([...pantry, processed_txt]);
+      } catch (error) {
+        console.error("Error adding item:", error);
+      }
+      setNewItem("");
+    }
+  }
+  
+  async function deleteItem() {
+    const processed_txt = newItem.trim().toLowerCase();
+    if (processed_txt && pantry.includes(processed_txt)) {
+      try {
+        const itemDoc = doc(firestore, "pantry", processed_txt);
+        await deleteDoc(itemDoc);
+        setPantry(pantry.filter((item) => item !== processed_txt));
+      } catch (error) {
+        console.error("Error deleting item:", error);
+      }
+      setNewItem("");
+    }
+  }
+  
+
+  const handleEditClick = (index, value) => {
+    setEditIndex(index);
+    setInputValue(value);
+  };
+
+  const handleInputChange = (event) => {
+    const processed_txt = event.target.value.trim().toLowerCase();
+    setInputValue(processed_txt);
+
+    setPantry((prevPantry) => {
+      const updatedPantry = [...prevPantry];
+      updatedPantry[editIndex] = processed_txt;
+      return updatedPantry;
+    });
+  };
+
+  const handleInputBlur = () => {
+    setEditIndex(null);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex flex-wrap w-screen h-screen">
+      <div className="flex absolute w-full bg-black text-4xl font-serif p-2 justify-center">
+        <span className="text-white">
+          <BreakfastDiningIcon /> Pantry Tracker{" "}
+        </span>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="flex w-1/3 h-full" />
+      <div className="flex flex-col w-1/3 h-full justify-center gap-4">
+        <input
+          className="w-full bg-gray-300 rounded-md p-2 placeholder:text-black"
+          placeholder="Add/Delete Item"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
         />
+        <List>
+          <Stack direction="row" spacing={2}>
+            <ListItem className="updater-list-item" onClick={addItem}>
+              Add
+            </ListItem>
+            <ListItem className="updater-list-item" onClick={deleteItem}>
+              Delete
+            </ListItem>
+          </Stack>
+        </List>
+
+        <input
+          className="w-full bg-gray-300 rounded-md p-2 placeholder:text-black"
+          placeholder="Search Pantry"
+          value={searchQuery}
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+        />
+
+        <List>
+          <div>Pantry List</div>
+          {filteredPantry.map((element, index) => (
+            <ListItem
+              key={index}
+              className="list-item"
+              onClick={() => handleEditClick(index, element)}
+            >
+              <div className="flex justify-between items-center">
+                {editIndex === index ? (
+                  <TextField
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onBlur={handleInputBlur}
+                    autoFocus
+                    size="small"
+                    variant="outlined"
+                  />
+                ) : (
+                  <span> {element} </span>
+                )}
+                <IconButton onClick={() => handleEditClick(index, element)}>
+                  <EditIcon />
+                </IconButton>
+              </div>
+            </ListItem>
+          ))}
+        </List>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
